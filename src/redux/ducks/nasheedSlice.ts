15bebc97e7197b848046763596d400028c9e8253
Mohammed Ali <mohaali482@gmail.com>
2023-04-18 18:02:11 +0300

    Exported the nasheed interface

--- src/redux/ducks/nasheedSlice.ts
+++ src/redux/ducks/nasheedSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-interface Nasheed {
+export interface Nasheed {
   id: number;
   owner: string;
   name: string;
