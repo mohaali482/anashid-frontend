b4886b036e4c4c6c2dd80bc7a2640fb7ffc9b7d0
Mohammed Ali <mohaali482@gmail.com>
2023-04-18 18:08:27 +0300

    Added sagas as a middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import nasheedsReducer from "./ducks/nasheedSlice";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas/saga";
+
+let sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
 export const store = configureStore({
   reducer: {
     nasheeds: nasheedsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
