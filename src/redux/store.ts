cf470d6f45ffbf4f6a752936e986be47f6cbb308
Mohammed Ali <mohaali482@gmail.com>
2023-04-21 14:17:33 +0300

    Made the values of the store non serialized

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,7 +11,7 @@ export const store = configureStore({
     nasheeds: nasheedsReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware),
+    getDefaultMiddleware({ serializableCheck: false }).concat(middleware),
 });
 
 sagaMiddleware.run(rootSaga);
