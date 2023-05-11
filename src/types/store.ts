eca947c2ffc15a7ab09d685cb2cfde1158b54ab7
Mohammed Ali <mohaali482@gmail.com>
2023-05-11 08:12:26 +0300

    Moved store types to store.ts

---
+++ src/types/store.ts
@@ -0,0 +1,45 @@
+export interface Response {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Nasheed[];
+}
+
+export interface Owner {
+  id: number;
+  username: string;
+  first_name: string | null;
+  last_name: string | null;
+  email: string | null;
+  image: File | null;
+  date_joined: Date;
+}
+
+export interface Nasheed {
+  id: number;
+  owner: Owner;
+  name: string;
+  poster: string;
+  audio: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface NasheedError {
+  name: Array<string>;
+  poster: Array<string>;
+  audio: Array<string>;
+}
+
+export interface NasheedsState {
+  items: Array<Nasheed>;
+  nasheed: Nasheed | null;
+  loading: boolean;
+  formErrors: NasheedError | null;
+  error: string | null;
+  next: string | null;
+  previous: string | null;
+  limit: number;
+  loadedIds: Array<number>;
+  loadMoreLoading: boolean;
+}
