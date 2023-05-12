97f9ad562817265ae6afc6fa6ffe98b9b6d8236b
Mohammed Ali <mohaali482@gmail.com>
2023-05-12 22:09:18 +0300

    Added configuration for react router and styled components theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from './App'
 import './index.css'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './components/styled/theme'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={defaultTheme} >
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
