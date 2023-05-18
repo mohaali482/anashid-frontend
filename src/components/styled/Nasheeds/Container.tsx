3ad86fab441f154d5efbcda557dc2b623b17a647
Mohammed Ali <mohaali482@gmail.com>
2023-05-18 08:55:46 +0300

    Added a container for the view

---
+++ src/components/styled/Nasheeds/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0.5rem;
+
+  @media (min-width: 1024px) {
+    margin-left: 16rem;
+  }
+`
+
+export default Container;
