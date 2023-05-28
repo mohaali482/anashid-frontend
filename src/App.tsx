7d8cb7898d2fe56a1ed1d55078756280940512b5
Mohammed Ali <mohaali482@gmail.com>
2023-05-28 09:15:45 +0300

    Moved the router to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import router from './routes/index';
+import Router from './routes/index';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [open, setOpen] = useState(false)
+  // const toggleDrawer = () => {
+  //   setOpen(!open)
+  // }
+
+  // useEffect(() => {
+  //   if (open) {
+  //     document.body.style.overflow = 'hidden'
+  //   }
+  //   else {
+  //     document.body.style.overflow = 'unset'
+  //   }
+  // }, [open])
+
+  // useEffect(() => {
+  //   document.addEventListener('keydown', (event) => {
+  //     if (event.key == 'Escape')
+  //       setOpen(false)
+  //   })
+  // 
+  //   return () => {
+  //     document.removeEventListener('keydown', (event) => {
+  //       if (event.key == 'Escape')
+  //         setOpen(false)
+  //     })
+
+  //   }
+  // }, [])
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <BrowserRouter >
+        <Router />
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,15 @@ import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './components/styled/theme'
 import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/AuthRouter';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme} >
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
