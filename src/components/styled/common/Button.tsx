a3c77d80149c39aa138dd643de80b8fc4f11b096
Mohammed Ali <mohaali482@gmail.com>
2023-05-28 09:16:53 +0300

    Added common components

---
+++ src/components/styled/common/Button.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    background-color: ${(props) => props.theme.palette.primary.main};
+    color: ${(props) => props.theme.palette.primary.dark};
+    border: none;
+    border-radius: ${(props) => props.theme.borderRadius};
+    padding: .5rem 5rem;
+    font-size: 1rem;
+    font-weight: bold;
+
+    @media (prefers-color-scheme: dark) {
+        color: ${(props) => props.theme.palette.primary.light};
+    }
+`
+
+export default StyledButton
---
+++ src/components/styled/common/Text.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledText = styled.p`
+    color: ${props => props.theme.palette.primary.main};
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin: 0;
+`
+
+export default StyledText;
