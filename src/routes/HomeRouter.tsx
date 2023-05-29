45d9c2e1438da91ce357a4b11e10236bbf546038
Mohammed Ali <mohaali482@gmail.com>
2023-05-29 14:55:53 +0300

    Added create nasheed to home router

--- src/routes/HomeRouter.tsx
+++ src/routes/HomeRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import List from "../pages/home/List";
 import HomeSharedLayout from "../pages/home/HomeSharedLayout";
+import CreateNasheede from "../pages/home/CreateNasheed";
 
 const HomeRouter = () => {
     return (
         <Routes>
             <Route element={<HomeSharedLayout />}>
                 <Route path="" index element={<List />} />
+                <Route path="add" element={<CreateNasheede />} />
             </Route>
         </Routes>
     )
