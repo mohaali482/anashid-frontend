de745555346b023310f7ef79014adabc2d1e7644
Mohammed Ali <mohaali482@gmail.com>
2023-07-09 16:57:23 +0300

    Change the width of the player

--- src/components/styled/Nasheeds/AudioPlayer.tsx
+++ src/components/styled/Nasheeds/AudioPlayer.tsx
@@ -12,14 +12,15 @@ export const StyledAudioPlayer = styled.audio`
 const StyledAudioPlayerContainer = styled.div`
     display: flex;
     align-items: center;
-    width: 100%;
+    width: -webkit-fill-available;
     height: 4rem;
     background-color:  ${(props) => props.theme.palette.primary.light};
     position: fixed;
     bottom: 0;
-
+    border-radius: ${(props) => props.theme.borderRadius};
+    margin-right: 0.5rem;
     z-index: 100;
-
+    
     @media (prefers-color-scheme: dark) {
         background-color: ${(props) => props.theme.palette.primary.dark};
     }
