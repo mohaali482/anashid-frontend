c75ecd2ebcba325e9af8546c6cf393ce5002672e
Mohammed Ali <mohaali482@gmail.com>
2023-08-02 18:29:38 +0300

    Fix: align search icon to be centered

--- src/components/styled/common/form/StyledIcon.tsx
+++ src/components/styled/common/form/StyledIcon.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 const StyledIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
     color: ${(props) => props.theme.palette.primary.main};
 `
 
