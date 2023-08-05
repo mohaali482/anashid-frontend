4a6ced2f71c9551da470d5f3589113c5e5cc53d5
Mohammed Ali <mohaali482@gmail.com>
2023-08-05 12:04:31 +0300

    Change detail header on small screens

--- src/components/styled/pages/detail/header.tsx
+++ src/components/styled/pages/detail/header.tsx
@@ -5,6 +5,15 @@ const FlexDiv = styled.div`
     gap: 1rem;
     align-items: center;
     width: 100%;
+
+    @media(max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+
+        & > * {
+            margin: 0;
+        }
+    }
 `
 
 export default FlexDiv;
