9732e40f0bd23761ea4f8e14b8832d06d0d2cb63
Mohammed Ali <mohaali482@gmail.com>
2023-08-15 15:32:52 +0300

    Add cache support for nasheeds services

--- src/services/nasheeds.ts
+++ src/services/nasheeds.ts
@@ -1,24 +1,51 @@
 import { Response } from "../types/store";
 import axios from "./config";
 
+let nasheeds_cache_keys = new Set<string>();
+
 export async function requestListNasheeds(
   limit: number,
   query: string
 ): Promise<Response> {
+  nasheeds_cache_keys.add(`list-nasheeds-limit-${limit}-query-${query}`);
   return (
-    await axios(`/nasheed/nasheeds/?limit=${limit}&name__contains=${query}`)
+    await axios(`/nasheed/nasheeds/?limit=${limit}&name__contains=${query}`, {
+      id: `list-nasheeds-limit-${limit}-query-${query}`,
+      cache: {
+        update: {
+          "list-nasheeds": () => {
+            console.log(nasheeds_cache_keys);
+            return "ignore";
+          },
+        },
+      },
+    })
   ).data;
 }
 
 export async function requestPageNasheeds(link: string): Promise<Response> {
-  return (await axios(link)).data;
+  nasheeds_cache_keys.add(`list-nasheeds-link-${link}`);
+  return (
+    await axios(link, {
+      id: `list-nasheeds-link-${link}`,
+    })
+  ).data;
 }
 
 export async function requestAddNasheed(formData: FormData) {
-  return await axios("/nasheed/nasheeds/", {
+  const response = await axios("/nasheed/nasheeds/", {
     data: formData,
     method: "POST",
   });
+
+  if (response.status === 201) {
+    nasheeds_cache_keys.forEach(async (key) => {
+      await axios.storage.remove(key);
+    });
+    nasheeds_cache_keys.clear();
+  }
+
+  return response;
 }
 
 export async function requestNasheed(id: number): Promise<Response> {
