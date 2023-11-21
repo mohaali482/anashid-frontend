c9b3f0657c77535527db7072e7875f9b6d7ea21b
Mohammed Ali <mohaali482@gmail.com>
2023-11-21 07:50:45 +0300

    Made audio player to extend to a drawer

--- src/components/styled/Nasheeds/AudioPlayer.tsx
+++ src/components/styled/Nasheeds/AudioPlayer.tsx
@@ -1,5 +1,50 @@
-import { useEffect, useRef } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import { IoClose } from 'react-icons/io5';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+
+
+
+const AudioDrawerPoster = styled.img`
+    width: 300px;
+    height: 300px;
+    object-fit: contain;
+    object-position: center;
+    border-radius: ${(props) => props.theme.borderRadius};
+`
+
+const AudioDrawerTitle = styled.h3`
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+    text-align: center;
+    color: ${(props) => props.theme.palette.primary.dark};
+
+    @media (prefers-color-scheme: dark) {
+        color: ${(props) => props.theme.palette.primary.lightGray};
+    }
+`
+
+
+const StyledCloseButton = styled.button`
+    color: ${(props) => props.theme.palette.primary.main};
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    cursor: pointer;
+    background: none;
+    border: none;
+    z-index: 1;
+`
+
+const Description = styled.div`
+    max-width: 40rem;
+    margin: 0 auto;
+    padding-bottom: 5rem;
+    padding-top: 5rem;
+`
+
 
 export const StyledAudioPlayer = styled.audio`
     width: 100%;
@@ -10,19 +55,26 @@ export const StyledAudioPlayer = styled.audio`
     }
 `;
 
-const StyledAudioPlayerContainer = styled.div`
+const StyledAudioPlayerContainer = styled.div<{ open: boolean }>`
     display: flex;
+    justify-content: center;
     align-items: center;
+    flex-direction: ${props => props.open ? "column" : "row"};
     width: -webkit-fill-available;
     width: -moz-available;
     width: fill-available;
-    height: 4rem;
+    width: ${props => props.open ? "100vw" : ""};
+    height: ${props => props.open ? "100vh" : "4rem"};
     background-color:  ${(props) => props.theme.palette.primary.light};
     position: fixed;
     bottom: 0;
+    left: ${props => props.open ? "0" : ""};
+    right: ${props => props.open ? "0" : ""};
     border-radius: ${(props) => props.theme.borderRadius};
     margin-right: 0.5rem;
     z-index: 100;
+    transition: all 0.5s ease;
+    overflow-y: ${(props) => props.open ? "scroll" : ""};
     
     @media (prefers-color-scheme: dark) {
         background-color: ${(props) => props.theme.palette.primary.dark};
@@ -31,25 +83,123 @@ const StyledAudioPlayerContainer = styled.div`
 
 const StyledAudioPoster = styled.img`
     max-height: 80%;
-    object-fit: contain;
+    object-fit: cover;
     padding: 1rem;
+`
+
+const Drawer = styled.div<{ open: boolean }>`
+    ${props => props.open ? "position: absolute; top: 1rem; left: 0; right: 0;" : "width: 100%"}
+`
+
+const AudioPlayerContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
 `
 
 interface AudioPlayerProps {
     audioRef: React.RefObject<HTMLAudioElement>;
     audio: string;
     poster: string;
-    onClick: any;
+    open: boolean;
+    setDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const AudioPlayer = (props: AudioPlayerProps) => {
+interface InternalAudioPlayerProps {
+    audioRef: React.RefObject<HTMLAudioElement>;
+    audio: string;
+    open: boolean;
+}
+
+const InternalAudioPlayer = (props: InternalAudioPlayerProps) => {
     return (
-        <StyledAudioPlayerContainer onClick={props.onClick}>
-            <StyledAudioPoster src={props.poster} />
-            <StyledAudioPlayer controls ref={props.audioRef}>
-                <source src={props.audio} type="audio/mpeg" />
-            </StyledAudioPlayer>
-        </StyledAudioPlayerContainer>
+        <StyledAudioPlayer controls ref={props.audioRef} style={props.open ? { width: "70%", padding: "1rem" } : undefined}>
+            <source src={props.audio} type="audio/mpeg" />
+        </StyledAudioPlayer >
+    )
+}
+
+const MemoizedAudioPlayer = memo(InternalAudioPlayer)
+
+const AudioPlayer = (props: AudioPlayerProps) => {
+    const { currentPlaying } = useSelector((state: RootState) => state.nasheeds)
+    if (props.open) {
+        document.body.style.overflow = 'hidden';
+        document.title = "Nasheeds - " + currentPlaying?.name
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+    useEffect(() => {
+        const keyboardEventListener = (ev: KeyboardEvent) => {
+            if (props.audioRef.current) {
+                switch (ev.key.toLowerCase()) {
+                    case " ":
+                    case "k":
+                        if (props.audioRef.current.paused) {
+                            props.audioRef.current.play()
+                        } else if (props.audioRef.current) {
+                            props.audioRef.current.pause();
+                        }
+                        break;
+                    case "arrowright":
+                        props.audioRef.current.currentTime += 5;
+                        break;
+                    case "l":
+                        props.audioRef.current.currentTime += 10;
+                        break;
+                    case "arrowleft":
+                        props.audioRef.current.currentTime -= 5;
+                        break
+                    case "j":
+                        props.audioRef.current.currentTime -= 10;
+                        break;
+                    case "f":
+                        props.setDrawerOpen((open) => !open)
+                        break;
+                    case "escape":
+                        props.setDrawerOpen(false)
+                        break;
+                }
+            }
+        }
+        if (currentPlaying !== null) {
+            document.body.addEventListener('keyup', keyboardEventListener)
+        }
+
+        return () => {
+            document.body.removeEventListener('keyup', keyboardEventListener)
+        }
+
+    }, [currentPlaying])
+    return (
+        <StyledAudioPlayerContainer open={props.open} onClick={() => props.audioRef.current !== null ?? props.setDrawerOpen(true)}>
+            {props.open &&
+                <StyledCloseButton>
+                    <IoClose size={30} />
+                </StyledCloseButton>}
+
+            {!props.open && <StyledAudioPoster src={props.poster} />}
+            <Drawer open={props.open}>
+                {props.open &&
+                    <center>
+                        <AudioDrawerPoster src={currentPlaying?.poster} alt="audio-poster" />
+                    </center>}
+                <AudioPlayerContainer>
+                    <MemoizedAudioPlayer audioRef={props.audioRef} audio={props.audio} open={props.open} />
+                </AudioPlayerContainer>
+
+                {props.open &&
+                    <>
+                        <div>
+                            <AudioDrawerTitle>{currentPlaying?.name}</AudioDrawerTitle>
+                        </div>
+                        <Description>
+                            {currentPlaying?.description}
+                        </Description>
+                    </>
+                }
+            </Drawer>
+        </StyledAudioPlayerContainer >
     )
 }
 
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,7 +7,6 @@ import Footer from "../components/styled/layout/Footer";
 import { IoAddCircle, IoHome, IoSave } from 'react-icons/io5'
 import { FaUserAlt } from 'react-icons/fa'
 import { BsMusicNoteList } from 'react-icons/bs'
-import AudioPlayerDrawer from "../components/styled/Nasheeds/AudioPlayerDrawer";
 import AudioPlayer from "../components/styled/Nasheeds/AudioPlayer";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -50,12 +49,10 @@ const AppLayout = () => {
     const { currentPlaying } = useSelector((state: RootState) => state.nasheeds)
 
     const audioRef = useRef<HTMLAudioElement>(null);
-    const audioRefDrawer = useRef<HTMLAudioElement>(null);
 
     useEffect(() => {
         if (currentPlaying?.audio) {
             audioRef.current?.load();
-            audioRefDrawer.current?.load();
             audioRef.current?.play();
         }
     }, [currentPlaying])
@@ -63,37 +60,15 @@ const AppLayout = () => {
 
     const [drawerOpen, setDrawerOpen] = useState(false)
     const drawerRef = useRef<HTMLDivElement>(null)
-    const toggleDrawer = () => {
-        setDrawerOpen(!drawerOpen)
-    }
 
     useEffect(() => {
         if (drawerOpen) {
             document.body.style.overflow = 'hidden'
-            audioRefDrawer.current!.currentTime = audioRef.current!.currentTime
-            audioRef.current!.pause()
         }
         else {
-            audioRef.current!.currentTime = audioRefDrawer.current!.currentTime
             document.body.style.overflow = 'unset'
-            audioRefDrawer.current!.pause()
         }
     }, [drawerOpen])
-
-    useEffect(() => {
-        document.addEventListener('keydown', (event) => {
-            if (event.key == 'Escape')
-                setDrawerOpen(false)
-        })
-
-        return () => {
-            document.removeEventListener('keydown', (event) => {
-                if (event.key == 'Escape')
-                    setDrawerOpen(false)
-            })
-
-        }
-    }, [])
 
     useEffect(() => {
         document.addEventListener('mousedown', (event) => {
@@ -124,9 +99,8 @@ const AppLayout = () => {
             <Container>
                 <Outlet />
                 <Footer />
-                <AudioPlayer audioRef={audioRef} audio={currentPlaying?.audio || ''} poster={currentPlaying?.poster || ''} onClick={toggleDrawer} />
+                <AudioPlayer audioRef={audioRef} audio={currentPlaying?.audio || ''} poster={currentPlaying?.poster || ''} setDrawerOpen={setDrawerOpen} open={drawerOpen} />
             </Container>
-            <AudioPlayerDrawer audioRef={audioRefDrawer} open={drawerOpen} toggleDrawer={toggleDrawer} reference={drawerRef} />
         </>
     )
 }
