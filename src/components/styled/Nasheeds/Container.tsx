83eded096c13f999a50926100f232efcaf7650da
Mohammed Ali <mohaali482@gmail.com>
2023-12-17 08:43:24 +0300

    Change height of container

--- src/components/styled/Nasheeds/Container.tsx
+++ src/components/styled/Nasheeds/Container.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Container = styled.div`
   padding: 0.5rem;
-  margin-top: 4rem;
-  height: calc(100% - 5rem);
+  margin-top: 5rem;
+  height: 100%;
   display: flex;
   flex-direction: column;
 
