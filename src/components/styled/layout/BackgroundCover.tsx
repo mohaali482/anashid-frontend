42a964ad0f052a3f2fade8da027b77b379e1cddc
Mohammed Ali <mohaali482@gmail.com>
2023-12-17 11:09:39 +0300

    Add a background color to make text readable

--- src/components/styled/layout/BackgroundCover.tsx
+++ src/components/styled/layout/BackgroundCover.tsx
@@ -14,6 +14,7 @@ const StyledDiv = styled.div`
     margin: 0 auto;
     max-width: 1280px;
     text-align: center;
+    background-color: rgba(0, 0, 0, 0.5);
 
     @media (min-width: 1024px) {
         padding-top: 9rem;
