d41f0ec9564aed705580325c277f054d44d5e61c
Mohammed Ali <mohaali482@gmail.com>
2023-12-17 08:53:04 +0300

    Change margin to padding

--- src/components/styled/Nasheeds/Container.tsx
+++ src/components/styled/Nasheeds/Container.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   padding: 0.5rem;
-  margin-top: 5rem;
+  padding-top: 5rem;
   height: 100%;
   display: flex;
   flex-direction: column;
